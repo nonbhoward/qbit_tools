d005153c5c17fbe22619de10474535308ecba6e0
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-01-25 02:40:30 -0500

    most popular result found, next step is to add result

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+from datetime import datetime
 from qbit_tasker import QbitTasker
 from time import sleep
 
 
 def mainloop():
+    start_application()
     qbt = QbitTasker()
     while True:
+        print('beginning new loop at {}'.format(datetime.now()))
         qbt.begin_queued_searches()
         qbt.parse_completed_searches()
         qbt.check_watched_downloads()
         qbt.transfer_files_to_remote()
-        exit()
-        sleep(60)
+        sleep(2)
+
+
+def start_application():
+    pass
 
 
 mainloop()
