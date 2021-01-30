4a6e84066ecb1ac6880c4c69bfde10f99db2e963
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-01-30 18:02:37 -0500

    refactored for new structure

--- main_loop.py
+++ main_loop.py
@@ -9,13 +9,14 @@ user_configuration = get_user_configuration()
 def mainloop():
     ml.log_event('main loop has started..')
     start_application()
-    qbit = QbitTasker(user_configuration)
+    qbit = QbitTasker(user_configuration=user_configuration,
+                      debug=True)
     while True:
         ml.log_event(event='new loop starting at {}'.format(datetime.now(), announce=True))
         qbit.initiate_and_monitor_searches()
         qbit.increment_loop_count()
         ml.log_event('main loop has ended, {} total loops..'.format(qbit.main_loop_count))
-        main_loop_wait = user_configuration.hardcoded.keys.pause_keys.MAIN_LOOP
+        main_loop_wait = user_configuration.hardcoded.keys.user_config_keys.MAIN_LOOP_WAIT
         qbit.pause_on_event(main_loop_wait)
 
 
