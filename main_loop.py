ed5e08fcdc03e77efc3e2abdb422129bec0f7e0f
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-01-31 02:15:01 -0500

    add try/except

--- main_loop.py
+++ main_loop.py
@@ -7,17 +7,20 @@ user_configuration = get_user_configuration()
 
 
 def mainloop():
-    ml.log_event('main loop has started..')
-    start_application()
-    qbit = QbitTasker(user_configuration=user_configuration,
-                      debug=True)
-    while True:
-        ml.log_event(event='new loop starting at {}'.format(datetime.now(), announce=True))
-        qbit.initiate_and_monitor_searches()
-        qbit.increment_loop_count()
-        ml.log_event('main loop has ended, {} total loops..'.format(qbit.main_loop_count))
-        main_loop_wait = user_configuration.hardcoded.keys.user_config_keyring.MAIN_LOOP
-        qbit.pause_on_event(main_loop_wait)
+    try:
+        ml.log_event('main loop has started..')
+        start_application()
+        qbit = QbitTasker(user_configuration=user_configuration,
+                          debug=True)
+        while True:
+            ml.log_event(event='new loop starting at {}'.format(datetime.now(), announce=True))
+            qbit.initiate_and_monitor_searches()
+            qbit.increment_loop_count()
+            ml.log_event('main loop has ended, {} total loops..'.format(qbit.main_loop_count))
+            main_loop_wait = user_configuration.hardcoded.keys.user_config_keyring.MAIN_LOOP
+            qbit.pause_on_event(main_loop_wait)
+    except Exception as e_err:
+        ml.log_event(e_err, level=ml.ERROR)
 
 
 def start_application():
