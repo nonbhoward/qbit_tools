9ac7903503ac757d580710796f59e27d8457cdf2
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-02-20 00:09:17 -0500

    simplify main_loop

--- main_loop.py
+++ main_loop.py
@@ -5,21 +5,21 @@ ml = MinimalLog()
 
 
 def main_loop():
-    try:
-        ml.log_event('\n\nmain loop has started..')
-        start_application()
-        qsm = QbitStateManager()
-        while True:
-            ml.log_event(event='new loop starting at {}'.format(datetime.now(), announce=True))
-            qsm.initiate_and_monitor_searches()
-            qsm.increment_loop_count()
-            qsm.pause_on_event(qsm.cfg.settings.WAIT_FOR_MAIN_LOOP)
-    except Exception as e_err:
-        ml.log_event(e_err, level=ml.ERROR)
+    qsm = start_application()
+    while True:
+        ml.log_event(event=f'new loop starting at {datetime.now()}', announce=True)
+        qsm.initiate_and_monitor_searches()
+        qsm.increment_loop_count()
+        qsm.pause_on_event(qsm.cfg.settings.WAIT_FOR_MAIN_LOOP)
 
 
 def start_application():
-    pass
+    try:
+        ml.log_event('\n\nmain loop has started..')
+        ml.log_event(f'TODO, start qbittorrent programatically', level=ml.WARNING)
+        return QbitStateManager()
+    except Exception as e_err:
+        ml.log_event(e_err, level=ml.ERROR)
 
 
 main_loop()
