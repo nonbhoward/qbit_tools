75bc39c5a689f06dc8f24b2eedce84cb565b61d9
nonbhowardST <55259160+nonbhoward@users.noreply.github.com>
2021-03-09 22:33:16 -0500

    restore settings to debug default

--- cfg_restore.sh
+++ cfg_restore.sh
@@ -7,6 +7,8 @@ git restore ./data_meta/metadata_added.cfg
 git restore ./data_meta/metadata_failed.cfg
 echo -e '\trestoring search configurations!'
 git restore ./data_search/search.cfg
+echo -e '\trestoring user settings!'
+git restore ./user_configuration/EDIT_SETTINGS_HERE.cfg
 echo -e '\tdeleting event log!'
 if [ -f event.log ]; then
 	rm event.log
