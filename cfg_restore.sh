f323a5b788b4de013e80d994549ba53ddedc046a
nonbhowardST <55259160+nonbhoward@users.noreply.github.com>
2021-03-09 21:20:13 -0500

    add debug script

---
+++ cfg_restore.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+clear
+echo -e 'starting configuration restore routine..'
+git status
+echo -e '\trestoring metadata configurations!'
+git restore ./data_meta/metadata_added.cfg
+git restore ./data_meta/metadata_failed.cfg
+echo -e '\trestoring search configurations!'
+git restore ./data_search/search.cfg
+echo -e '\tdeleting event log!'
+if [ -f event.log ]; then
+	rm event.log
+	echo -e '\tevent log removed!'
+else
+	echo -e '\tevent log not found!'
+fi
+git status
+echo -e '\t..done!'
