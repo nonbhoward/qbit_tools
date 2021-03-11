b6319ff71d67d442eaa9213632b924ffc8ec77ad
nonbhowardST <55259160+nonbhoward@users.noreply.github.com>
2021-03-11 00:30:16 -0500

    add ping function

--- network/mapper.py
+++ network/mapper.py
@@ -1,6 +1,6 @@
+from os import system
 from socket import AF_INET
 from socket import SOCK_DGRAM
-from socket import gethostbyname
 from socket import gethostname
 from socket import socket
 from minimalog.minimal_log import MinimalLog
@@ -9,17 +9,26 @@ ml = MinimalLog(__name__)
 
 class NetProbe:
     def __init__(self):
-        pass
+        ml.log_event(f'initializing \'{self.__class__.__name__}\'')
 
     @staticmethod
     def get_local_ip_address(alt_method=False) -> str:
         try:
-            if alt_method:
+            ml.log_event(f'fetching ip address from local computer')
+            if not alt_method:
                 s = socket(AF_INET, SOCK_DGRAM)
                 s.connect(('10.255.255.255', 1))
                 return s.getsockname()[0]
             return [(s.connect(('8.8.8.8', 53)), s.getsockname()[0],
                      s.close()) for s in [socket(AF_INET, SOCK_DGRAM)]][0][1]
+        except Exception as e_err:
+            print(e_err.args[0])
+
+    @staticmethod
+    def ping_response_received_from_(ip_address):
+        try:
+            ml.log_event(f'pinging ip address at \'{ip_address}\'')
+            return system('ping -c 1 ' + ip_address + ' >/dev/null') == 0
         except Exception as e_err:
             print(e_err.args[0])
 
@@ -37,7 +46,6 @@ class LocalServer:
         self.properties = {
             'host name':    gethostname(),
             'ip address':   self.probe.get_local_ip_address(),
-            'ip alt':       self.probe.get_local_ip_address(alt_method=True)
         }
 
 
