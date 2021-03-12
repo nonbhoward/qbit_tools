bdd06a2502cccc06a822e1e6fbb7ca47ba57b698
nonbhowardST <55259160+nonbhoward@users.noreply.github.com>
2021-03-11 20:00:55 -0500

    expand error handling

--- network/dispatcher.py
+++ network/dispatcher.py
@@ -4,11 +4,16 @@ ml = MinimalLog(__name__)
 
 class Operator:
     def __init__(self):
-        pass
+        try:
+            pass
+        except Exception as e_err:
+            ml.log_event(f'error initializing \'{self.__class__.__name__}\'', level=ml.ERROR)
+            ml.log_event(e_err.args[0], level=ml.ERROR)
 
     @staticmethod
     def transfer_files_to_remote():
         try:
             pass
         except Exception as e_err:
+            ml.log_event(f'error transferring files to remote', level=ml.ERROR)
             ml.log_event(e_err.args[0], level=ml.ERROR)
