4f96bdcbea2c533e1bf991bd27714ffbd30ca0ed
nonbhowardST <55259160+nonbhoward@users.noreply.github.com>
2021-03-13 14:40:19 -0500

    remove redundant level

--- extra/log_mgmt.py
+++ extra/log_mgmt.py
@@ -7,7 +7,7 @@ class LogManager:
         try:
             pass
         except Exception as e_err:
-            ml.log_event(f'error initializing \'{self.__class__.__name__}\'', level=ml.ERROR)
+            ml.log_event(f'error initializing \'{self.__class__.__name__}\'')
             ml.log_event(e_err.args[0], level=ml.ERROR)
 
     @staticmethod
