0aaebdbece979f577557bb49ed1a6fbe13ef0d21
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-04-13 21:29:20 -0400

    refactor: remove incomplete code

--- main_loop.py
+++ main_loop.py
@@ -14,44 +14,16 @@ u_key = QConf.get_keyring_for_(settings=True)
 
 def main_loop():
     ml.log('main loop has started..', announcement=True)
-    qsm = start_application_and_return_state_machine()
+    qbit_state_machine = initialize_and_return_state_machine()
     while True:
         ml.log(f'new loop starting at {datetime.now()}', announcement=True)
-        qsm.initiate_and_monitor_searches()
-        qsm.increment_main_loop_count()
+        qbit_state_machine.initiate_and_monitor_searches()
+        qbit_state_machine.increment_main_loop_count()
         pause_on_event(u_key.WAIT_FOR_MAIN_LOOP)
 
 
-def application_is_running(app_path: Path) -> bool:
+def initialize_and_return_state_machine():
     try:
-        return True  # FIXME p3
-        cmd = 'ps aux'
-        data = launch(cmd, capture_output=True)
-        data = [(int(p), c) for p, c in [x.rstrip(']n').split(' ', 1) for x in Popen('ps h -eo pid:1,command')]]
-        ps_path = '/proc'
-        app_name, ps = str(app_path).split('/')[-1], ls(ps_path)
-        if app_name in ps:
-            return True
-        return False
-    except Exception as e_err:
-        ml.log(f'error checking if application is running')
-        ml.log(e_err.args[0], level=ml.ERROR)
-
-
-def start_application_and_return_state_machine():
-    try:
-        # TODO qbit could be in other locations.. /opt etc
-        std_path, app_name = ['usr', 'bin'], 'qbittorrent'
-        app = Path(Path(cwd()).root).joinpath(*std_path, app_name)
-        if not supported_os():
-            ml.log('unsupported OS')
-            exit()
-        if application_is_running(app):
-            return QbitStateMachine()
-        if not exists(app):
-            ml.log('install qbittorrent')
-            exit()
-        launch(app)
         return QbitStateMachine()
     except Exception as e_err:
         ml.log(f'error starting application and fetching state machine')
@@ -60,11 +32,7 @@ def start_application_and_return_state_machine():
 
 def supported_os() -> bool:
     try:
-        nightmare_hellscape = 'win32'
-        if nightmare_hellscape in platform:
-            print('self-harm detected, program ending')
-            exit()
-        supported = ['linux']
+        supported = ['linux', 'win32']
         if platform in supported:
             return True
         return False
