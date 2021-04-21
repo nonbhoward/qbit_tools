6d826ababc9e3124e447ccf37541a401c4d97c83
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-04-20 23:17:27 -0400

    add: reset that keeps developer search & settings

--- devel/reset_project_data.sh
+++ devel/reset_project_data.sh
@@ -6,8 +6,7 @@ echo "re-creating soft-link content from parallel directory"
 ln -s ../minimalog ./minimalog
 echo "operation finished"
 git status
-if [ -f 'event.log' ]
-then
+if [ -f 'event.log' ];then
   echo 'removing event.log'
   rm event.log
 else
@@ -19,5 +18,21 @@ echo 'restoring metadata_failed.cfg'
 git restore data_meta/metadata_failed.cfg
 echo 'restoring search.cfg'
 git restore data_search/search.cfg
+if [ -f './devel/search.cfg' ];then
+  echo 'developer search.cfg found'
+  cp ./devel/search.cfg ./data_search/search.cfg
+  echo 'attempted to overwrite project search.cfg with developer version'
+else
+  echo 'developer search.cfg not found'
+  echo 'doing nothing'
+fi
+if [ -f './devel/EDIT_SETTINGS_HERE.cfg' ];then
+  echo 'developer settings found'
+  cp ./devel/EDIT_SETTINGS_HERE.cfg ./user_configuration/EDIT_SETTINGS_HERE.cfg
+  echo 'attempted to overwrite project EDIT_SETTINGS_HERE.cfg with developer version'
+else
+  echo 'developer EDIT_SETTINGS_HERE.cfg not found'
+  echo 'doing nothing'
+fi
 git log --pretty=oneline -n8
 git status
